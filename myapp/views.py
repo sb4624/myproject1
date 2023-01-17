4c764ca94068e25a786e44daba3cb3cfbaea61d9
백시연 <lia@Siyeonier.local>
2023-01-17 15:11:34 -0500

    Initial commit

---
+++ myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+        data = dict()
+        import datetime
+        time = datetime.datetime.now()
+        data["time_of_day"] = time
+        return render(request, "home.html", context=data)
